9f9280586ddd0e62a26ac7158f31c016b46622c7
Ricardo Davids <ricardo.david2020.new@gmail.com>
2020-12-08 14:20:54 +0000

    First Model-controller-View - Department

--- Vendas_MVC/Controllers/DepartmentsController.cs
+++ Vendas_MVC/Controllers/DepartmentsController.cs
@@ -12,9 +12,10 @@ namespace Vendas_MVC.Controllers
         public IActionResult Index()
         {
             List<Department> list = new List<Department>();
-            list.Add(new Department { Id = 1, Name = "Electronics" });
-            list.Add(new Department { Id = 2, Name = "Fashion" });
+            list.Add(new Department { Id = 1, Name = "Electronics" }); // Aqui estão criado os objectos do meu controlador como por exemplo:. Name = "Electronics" 
+            list.Add(new Department { Id = 2, Name = "Fashion" });// Aqui estão criado os objectos do meu controlador
 
+            // Agora esses objectos são passados para View
             return View(list);
         }
     }
