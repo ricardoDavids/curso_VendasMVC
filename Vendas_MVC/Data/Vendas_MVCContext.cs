5c00fa0129f83c0bb3f86def7f9abe350ac9363d
Ricardo Davids <ricardo.david2020.new@gmail.com>
2020-12-08 19:43:07 +0000

    CRUD SCAFFOLDING

---
+++ Vendas_MVC/Data/Vendas_MVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Vendas_MVC.Models;
+
+namespace Vendas_MVC.Data
+{
+    public class Vendas_MVCContext : DbContext
+    {
+        public Vendas_MVCContext (DbContextOptions<Vendas_MVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Vendas_MVC.Models.Department> Department { get; set; }
+    }
+}
--- Vendas_MVC/Startup.cs
+++ Vendas_MVC/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Vendas_MVC.Data;
 
 namespace Vendas_MVC
 {
@@ -33,6 +35,9 @@ namespace Vendas_MVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<Vendas_MVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("Vendas_MVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
